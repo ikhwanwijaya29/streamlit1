0b699d56d0ba58eed54dba61e421c00862f7257c
ikhwanwijaya29 <88754762+ikhwanwijaya29@users.noreply.github.com>
2023-11-29 14:15:03 +0700

    Update dashboard_bike_sharing(1).py

--- dashboard_bike_sharing(1).py
+++ dashboard_bike_sharing(1).py
@@ -12,7 +12,7 @@ import plotly.graph_objects as go
 # LOAD DATA
 # ==============================
 
-@st.cache_resource
+@st.cache
 def load_data():
     data = pd.read_csv("streamlit1/hour.csv")
     return data
